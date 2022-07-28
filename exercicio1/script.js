e114b50ff14285dff7504f24d44a873c4309455b
rodolfofreitass <rodolfofreitas38@hotmail.com>
2022-07-28 20:52:28 -0300

    Exercicio A010

---
+++ exercicio1/script.js
@@ -0,0 +1,5 @@
+const numeros = [2, -4, 9, 1, -12, 5, 20]
+const strings = ['carros','motos','barcos','avioes']
+const mistos = [38, 11, -23, 'youtube', 'instagram', false, true]
+
+console.log(numeros, strings, mistos)
